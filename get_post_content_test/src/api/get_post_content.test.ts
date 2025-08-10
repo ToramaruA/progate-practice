42265575fb95f27327f7c2505cf0184f7b8e7bb5
Toramaru <tigermaru_new@keio.jp>
2025-08-10 14:08:56 +0900

    add get_post_content_test practice

---
+++ get_post_content_test/src/api/get_post_content.test.ts
@@ -0,0 +1,34 @@
+import {getPostContent} from "@/api/get_post_content";
+import {fetchPost} from "@/api/fetch_post";
+
+jest.mock("@/api/fetch_post", () => {
+  return {
+    fetchPost: jest.fn().mockImplementation((id: number) => {
+      if (id === 1) {
+        return Promise.resolve({content: "Mock Post Content"});
+      } else {
+        return Promise.resolve(null);
+      }
+    }),
+  };
+});
+
+describe("getPostContent", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("returns null on fetchPost failure", async () => {
+    const ret = await getPostContent(-1);
+    expect(ret).toBeNull();
+    expect(fetchPost).toHaveBeenCalledWith(-1);
+    expect(fetchPost).toBeCalledTimes(1);
+  });
+
+  test("returns content on fetchPost success", async () => {
+    const ret = await getPostContent(1);
+    expect(ret).toBe("Mock Post Content");
+    expect(fetchPost).toHaveBeenCalledWith(1); // fetchPostが1を引数に呼ばれたかを確認
+    expect(fetchPost).toBeCalledTimes(1); // fetchPostが1回呼ばれたことを確認
+  });
+});
